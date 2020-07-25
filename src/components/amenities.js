8e2d9d8792d06d98ef8d6f7847fc100482c36554
Abir Tawfique <easternselling@gmail.com>
2020-07-25 16:43:13 -0400

    V1 of section header completed.

--- src/components/amenities.js
+++ src/components/amenities.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Amenities = () => {
     return (
         <div className="amenities">
-            <h2>Our Amenities</h2>
+            <div class="lines">
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+            <h2>Rooms and pricing</h2>
+            <span className='devider'></span>
+            <h5>All of our rooms feature a smart tv, computer desk and a dresser.</h5>
         </div>
     )
 }
