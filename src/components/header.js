8ef712bbf4cef5ea31e318d4615d7a054cc13371
A Tawfique <easternselling@gmail.com>
2020-08-02 20:50:36 -0400

    [Header] Added href ID tags

--- src/components/header.js
+++ src/components/header.js
@@ -2,25 +2,25 @@ import React from 'react'
 import logo from '../assets/logo2.png'
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="section-1">
-                <div className="logo">
-                    <img src={logo} alt="Logo"></img>
-                </div>
-            </div>
-            <div className='section-mid'>
-                <nav>
-                    <a href="#">Rooms & Pricing</a>
-                    <a href="#">About us</a>
-                    <a href="#">Frequently Asked Q's</a>
-                </nav>
-            </div>
-            <div className="section-2">
-                <button class="snip1372">Book Now<i class="ion-ios-calendar-outline"></i></button>
-            </div>
+  return (
+    <div className="header">
+      <div className="section-1">
+        <div className="logo">
+          <img src={logo} alt="Logo"></img>
         </div>
-    )
+      </div>
+      <div className='section-mid'>
+        <nav>
+          <a href="#rooms">Rooms & Pricing</a>
+          <a href="#">About us</a>
+          <a href="#faq">Frequently Asked Q's</a>
+        </nav>
+      </div>
+      <div className="section-2">
+        <button class="snip1372">Book Now<i class="ion-ios-calendar-outline"></i></button>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
